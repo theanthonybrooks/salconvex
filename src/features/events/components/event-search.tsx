118355df9d86021bf1cb4fd2fe366424e395657e
theanthonybrooks <61633975+theanthonybrooks@users.noreply.github.com>
2025-04-02 05:46:28 +0200

    fix: orgSearch type corrections

--- src/features/events/components/event-search.tsx
+++ src/features/events/components/event-search.tsx
@@ -1,32 +1,32 @@
-import { cn } from "@/lib/utils"
-import { useQuery } from "convex/react"
-import { AnimatePresence, motion } from "framer-motion"
-import { Check, CircleCheck, CircleX, Search } from "lucide-react"
-import Image from "next/image"
-import { useEffect, useRef, useState } from "react"
-import { api } from "~/convex/_generated/api"
-import { Doc } from "~/convex/_generated/dataModel"
+import { cn } from "@/lib/utils";
+import { useQuery } from "convex/react";
+import { AnimatePresence, motion } from "framer-motion";
+import { Check, CircleCheck, CircleX, Search } from "lucide-react";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import { api } from "~/convex/_generated/api";
+import { Doc } from "~/convex/_generated/dataModel";
 
 interface OrgSearchProps {
   value:
     | string
     | {
-        organizationName: string
+        organizationName: string;
       }
-    | null
+    | null;
   onChange: (
     value:
       | string
       | {
-          organizationName: string
+          organizationName: string;
         }
-      | null
-  ) => void
-  placeholder?: string
-  className?: string
-  isValid?: string
-  onReset?: () => void
-  onLoadClick: (org: Doc<"organizations"> | null) => void
+      | null,
+  ) => void;
+  placeholder?: string;
+  className?: string;
+  isValid?: string;
+  onReset?: () => void;
+  onLoadClick: (org: Doc<"organizations"> | null) => void;
 }
 
 export const OrgSearch = ({
@@ -39,72 +39,72 @@ export const OrgSearch = ({
   onLoadClick,
 }: OrgSearchProps) => {
   const [inputValue, setInputValue] = useState(
-    typeof value === "string" ? value : value?.organizationName ?? ""
-  )
+    typeof value === "string" ? value : (value?.organizationName ?? ""),
+  );
 
-  const invalid = isValid === "invalid"
-  const valid = isValid === "valid"
-  const [focused, setFocused] = useState(false)
-  const [clearHovered, setClearHovered] = useState(false)
-  const [hasUserInteracted, setHasUserInteracted] = useState(false)
-  const [selectedIndex, setSelectedIndex] = useState<number>(-1)
-  const [selectedVal, setSelectedVal] = useState<string>("")
-  const itemRefs = useRef<(HTMLLIElement | null)[]>([])
-  const containerRef = useRef<HTMLDivElement>(null)
-  const orgInputRef = useRef<HTMLInputElement>(null)
-  const trimmedQuery = inputValue.trim()
+  const invalid = isValid === "invalid";
+  const valid = isValid === "valid";
+  const [focused, setFocused] = useState(false);
+  const [clearHovered, setClearHovered] = useState(false);
+  const [hasUserInteracted, setHasUserInteracted] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState<number>(-1);
+  const [selectedVal, setSelectedVal] = useState<string>("");
+  const itemRefs = useRef<(HTMLLIElement | null)[]>([]);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const orgInputRef = useRef<HTMLInputElement>(null);
+  const trimmedQuery = inputValue.trim();
   const results = useQuery(
     api.organizer.organizations.getUserOrganizations,
-    hasUserInteracted ? { query: trimmedQuery || "" } : "skip"
-  )
-  const showSuggestions = focused && results && results.length > 0
+    hasUserInteracted ? { query: trimmedQuery || "" } : "skip",
+  );
+  const showSuggestions = focused && results && results.length > 0;
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newVal = e.target.value
-    setInputValue(newVal)
-    onChange(newVal)
-    onLoadClick(null)
-  }
+    const newVal = e.target.value;
+    setInputValue(newVal);
+    onChange(newVal);
+    onLoadClick(null);
+  };
 
   const handleSelect = (org: Doc<"organizations">) => {
-    console.log("organization: ", org)
-    onChange(org.organizationName)
-    setInputValue(org.organizationName)
-    setSelectedVal(org.organizationName)
-    onLoadClick(org)
-    setFocused(false)
-  }
+    console.log("organization: ", org);
+    onChange(org.name);
+    setInputValue(org.name);
+    setSelectedVal(org.name);
+    onLoadClick(org);
+    setFocused(false);
+  };
 
   const handleReset = () => {
-    setInputValue("")
-    onLoadClick(null)
-    if (value !== null) onChange(null)
-    if (onReset) onReset()
-    orgInputRef.current?.focus()
-  }
+    setInputValue("");
+    onLoadClick(null);
+    if (value !== null) onChange(null);
+    if (onReset) onReset();
+    orgInputRef.current?.focus();
+  };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (!results || results.length === 0) return
+    if (!results || results.length === 0) return;
 
     if (e.key === "ArrowDown") {
-      e.preventDefault()
-      setSelectedIndex((prev) => (prev + 1) % results.length)
+      e.preventDefault();
+      setSelectedIndex((prev) => (prev + 1) % results.length);
     } else if (e.key === "ArrowUp") {
-      e.preventDefault()
+      e.preventDefault();
       setSelectedIndex((prev) =>
         prev === -1
           ? results.length - 1
-          : (prev - 1 + results.length) % results.length
-      )
+          : (prev - 1 + results.length) % results.length,
+      );
     } else if (e.key === "Enter" && selectedIndex >= 0) {
-      e.preventDefault()
-      handleSelect(results[selectedIndex])
+      e.preventDefault();
+      handleSelect(results[selectedIndex]);
       // setSelectedVal(results[selectedIndex].organizationName)
     } else if (e.key === "Escape") {
-      setFocused(false)
-      setSelectedIndex(-1)
+      setFocused(false);
+      setSelectedIndex(-1);
     }
-  }
+  };
 
   useEffect(() => {
     if (
@@ -113,22 +113,23 @@ export const OrgSearch = ({
       "organizationName" in value &&
       inputValue !== value.organizationName
     ) {
-      setInputValue(value.organizationName)
+      setInputValue(value.organizationName);
     }
-  }, [value, inputValue])
+  }, [value, inputValue]);
 
   useEffect(() => {
-    const selectedEl = itemRefs.current[selectedIndex]
+    const selectedEl = itemRefs.current[selectedIndex];
     if (selectedEl) {
-      selectedEl.scrollIntoView({ behavior: "smooth", block: "nearest" })
+      selectedEl.scrollIntoView({ behavior: "smooth", block: "nearest" });
     }
-  }, [selectedIndex])
+  }, [selectedIndex]);
 
   return (
     <div
-      className='relative  max-w-sm lg:max-w-md lg:min-w-[400px] mx-auto'
-      ref={containerRef}>
-      <section className='relative z-[51]'>
+      className="relative mx-auto max-w-sm lg:min-w-[400px] lg:max-w-md"
+      ref={containerRef}
+    >
+      <section className="relative z-[51]">
         <input
           ref={orgInputRef}
           value={inputValue}
@@ -137,24 +138,24 @@ export const OrgSearch = ({
           onFocus={() => setFocused(true)}
           onBlur={() => setTimeout(() => setFocused(false), 100)}
           onKeyDown={(e) => {
-            if (!hasUserInteracted) setHasUserInteracted(true)
-            handleKeyDown(e)
+            if (!hasUserInteracted) setHasUserInteracted(true);
+            handleKeyDown(e);
           }}
           placeholder={placeholder}
           maxLength={35}
           className={cn(
-            " w-full border p-2 pl-8 pr-10 lg:pr-14 lg:pl-14 rounded",
+            "w-full rounded border p-2 pl-8 pr-10 lg:pl-14 lg:pr-14",
             className,
-            invalid && "text-red-500 italic ring-2 ring-red-500 ring-offset-1",
-            clearHovered && "text-red-500 italic line-through"
+            invalid && "italic text-red-500 ring-2 ring-red-500 ring-offset-1",
+            clearHovered && "italic text-red-500 line-through",
           )}
         />
         <Search
           onMouseDown={(e) => {
-            e.preventDefault()
-            containerRef.current?.querySelector("input")?.focus()
+            e.preventDefault();
+            containerRef.current?.querySelector("input")?.focus();
           }}
-          className='absolute left-2 top-1/2 transform -translate-y-1/2 text-foreground/50 hover:text-foreground/100 cursor-pointer size-4 lg:size-7 lg:ml-2 '
+          className="absolute left-2 top-1/2 size-4 -translate-y-1/2 transform cursor-pointer text-foreground/50 hover:text-foreground/100 lg:ml-2 lg:size-7"
         />
         {/* {focused && inputValue === "" && (
           <span className='absolute left-10 top-1/2 -translate-y-1/2 h-10 w-[2px] bg-foreground animate-caret-blink pointer-events-none' />
@@ -163,7 +164,7 @@ export const OrgSearch = ({
           <CircleCheck
             onMouseEnter={() => setClearHovered(true)}
             onMouseLeave={() => setClearHovered(false)}
-            className='text-emerald-600 font-bold absolute right-[9px] lg:right-4 top-1/2 -translate-y-1/2  cursor-pointer size-6 lg:size-7 '
+            className="absolute right-[9px] top-1/2 size-6 -translate-y-1/2 cursor-pointer font-bold text-emerald-600 lg:right-4 lg:size-7"
           />
         )}
         {(invalid || clearHovered) && (
@@ -172,8 +173,8 @@ export const OrgSearch = ({
             onMouseLeave={() => setClearHovered(false)}
             onClick={handleReset}
             className={cn(
-              " absolute right-[9px] lg:right-4 top-1/2 -translate-y-1/2 text-red-600 font-bold cursor-pointer size-6 lg:size-7 ",
-              clearHovered && "font-semibold spin-in-90 scale-110"
+              "absolute right-[9px] top-1/2 size-6 -translate-y-1/2 cursor-pointer font-bold text-red-600 lg:right-4 lg:size-7",
+              clearHovered && "scale-110 font-semibold spin-in-90",
             )}
           />
         )}
@@ -187,34 +188,35 @@ export const OrgSearch = ({
             exit={{ opacity: 0, height: 0 }}
             transition={{ duration: 0.25, ease: "easeInOut" }}
             layout
-            className='absolute z-50 top-full left-0 w-full bg-white shadow-md rounded-b max-h-52 overflow-auto border-x border-b pt-6 -translate-y-4 scrollable mini'
-            style={{ scrollPaddingTop: "1.2rem" }}>
+            className="scrollable mini absolute left-0 top-full z-50 max-h-52 w-full -translate-y-4 overflow-auto rounded-b border-x border-b bg-white pt-6 shadow-md"
+            style={{ scrollPaddingTop: "1.2rem" }}
+          >
             {results.map((org, idx) => (
               <li
                 key={org._id}
                 ref={(el) => {
-                  itemRefs.current[idx] = el
+                  itemRefs.current[idx] = el;
                 }}
                 onMouseDown={() => handleSelect(org)}
                 className={cn(
-                  "pr-2 pl-4 gap-x-4 cursor-pointer flex items-center relative py-3 ",
+                  "relative flex cursor-pointer items-center gap-x-4 py-3 pl-4 pr-2",
                   selectedIndex === idx
                     ? "bg-salYellow/50"
-                    : "hover:bg-salYellow/40"
-                )}>
+                    : "hover:bg-salYellow/40",
+                )}
+              >
                 {org.logo && (
                   <Image
                     src={org.logo}
-                    alt={org.organizationName}
+                    alt={org.name}
                     width={35}
                     height={35}
-                    className='rounded-full'
+                    className="rounded-full"
                   />
                 )}
-                {org.organizationName}
-                {org.organizationName.toLowerCase() ===
-                  selectedVal.toLowerCase() && (
-                  <Check className='absolute right-6 top-1/2 -translate-y-1/2 text-foreground font-bold ' />
+                {org.name}
+                {org.name.toLowerCase() === selectedVal.toLowerCase() && (
+                  <Check className="absolute right-6 top-1/2 -translate-y-1/2 font-bold text-foreground" />
                 )}
               </li>
             ))}
@@ -222,5 +224,5 @@ export const OrgSearch = ({
         )}
       </AnimatePresence>
     </div>
-  )
-}
+  );
+};
