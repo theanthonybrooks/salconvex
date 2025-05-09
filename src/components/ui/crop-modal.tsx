947684815f638af8d5e03cd5e30860a0f78cc5d4
theanthonybrooks <61633975+theanthonybrooks@users.noreply.github.com>
2025-05-09 04:18:41 +0200

    more avatar uploader tweaks (design-wise) adding some icons and generally just making it work a bit better. Also adjusted dashboard settings to use context as well as updating the form to have a reset that updates when the values change

--- src/components/ui/crop-modal.tsx
+++ src/components/ui/crop-modal.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button";
 import DiscreteSlider from "@/components/ui/slider";
 import { createImage, getCroppedImg, padImageToSquare } from "@/lib/imageFns";
+import { cn } from "@/lib/utils";
 import { useCallback, useEffect, useRef, useState } from "react";
 import Cropper, { Area } from "react-easy-crop";
+import { BiColorFill } from "react-icons/bi";
+import { PiResize } from "react-icons/pi";
+import { TbZoomInArea, TbZoomOutArea } from "react-icons/tb";
 
 type CropModalProps = {
   imageSrc: string;
@@ -18,6 +22,7 @@ export function CropModal({ imageSrc, onClose, onSave }: CropModalProps) {
   const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area | null>(null);
   const [paddingColor, setPaddingColor] = useState("#ffffff");
   const [paddingSize, setPaddingSize] = useState(100);
+  const [fitZoom, setFitZoom] = useState<number | null>(null);
 
   const onCropComplete = useCallback((_: Area, croppedArea: Area) => {
     setCroppedAreaPixels(croppedArea);
@@ -46,13 +51,12 @@ export function CropModal({ imageSrc, onClose, onSave }: CropModalProps) {
       const scaleY = padded.height / original.height;
       const scale = Math.max(scaleX, scaleY);
 
+      setFitZoom(scale);
       setZoom(scale);
       setInitialZoom.current = true;
     };
 
-    if (paddedImage) {
-      calculateInitialZoom();
-    }
+    calculateInitialZoom();
   }, [imageSrc, paddedImage]);
 
   return (
@@ -79,14 +83,15 @@ export function CropModal({ imageSrc, onClose, onSave }: CropModalProps) {
         </div>
 
         <div className="mt-4 flex flex-col items-center">
-          <label htmlFor="zoom" className="mb-1 text-sm text-foreground">
+          <label htmlFor="zoom" className="sr-only text-sm text-foreground">
             Zoom
           </label>
+
           <DiscreteSlider
             value={zoom}
             onChange={setZoom}
             marks={[
-              { value: 1, label: "Original" },
+              { value: 1, label: "Full" },
               { value: 2.5, label: "2.5x" },
               { value: 5, label: "5x" },
               { value: 10, label: "10x" },
@@ -103,9 +108,13 @@ export function CropModal({ imageSrc, onClose, onSave }: CropModalProps) {
           />
         </div>
         <div className="flex items-center justify-center gap-x-3">
-          <label htmlFor="color" className="mb-1 text-sm text-foreground">
+          <label
+            htmlFor="color"
+            className="sr-only mb-1 text-sm text-foreground"
+          >
             Border Color:
           </label>
+          <BiColorFill className="size-6 text-foreground" />
           <input
             id="color"
             type="color"
@@ -113,9 +122,13 @@ export function CropModal({ imageSrc, onClose, onSave }: CropModalProps) {
             onChange={(e) => setPaddingColor(e.target.value)}
             className="size-10 w-full max-w-40 cursor-pointer rounded-md border border-foreground px-2 py-1 text-sm shadow-sm focus:outline-none"
           />
-          <label htmlFor="color" className="mb-1 text-sm text-foreground">
+          <label
+            htmlFor="color"
+            className="sr-only mb-1 text-sm text-foreground"
+          >
             Padding:
           </label>
+          <PiResize className="size-6 text-foreground" />
           <input
             id="paddingSize"
             type="number"
@@ -126,6 +139,24 @@ export function CropModal({ imageSrc, onClose, onSave }: CropModalProps) {
             onChange={(e) => setPaddingSize(Number(e.target.value))}
             className="h-10 w-15 cursor-pointer rounded-md border border-foreground px-2 py-1 text-center text-sm shadow-sm focus:outline-none"
           />
+          {fitZoom && (
+            <button
+              type="button"
+              onClick={() => setZoom(fitZoom)}
+              className={cn(
+                "invisible px-2 hover:scale-105 hover:cursor-pointer active:scale-95",
+                zoom !== fitZoom && "visible",
+              )}
+            >
+              {zoom > fitZoom ? (
+                <TbZoomOutArea className="size-6 text-foreground" />
+              ) : (
+                <TbZoomInArea className="size-6 text-foreground" />
+              )}
+
+              <span className="sr-only">Zoom to fit</span>
+            </button>
+          )}
         </div>
         <div className="mt-4 flex justify-between">
           <Button type="button" variant="salWithShadowPink" onClick={onClose}>
