09d89706d107c93e7f34b6b79c9d7897ae6296f4
theanthonybrooks <61633975+theanthonybrooks@users.noreply.github.com>
2025-05-18 00:23:10 +0200

    meh

--- src/providers/posthog-provider.tsx
+++ src/providers/posthog-provider.tsx
@@ -8,14 +8,14 @@ import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host:
-        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
-      person_profiles: "identified_only",
-      capture_pageview: false,
-    });
-  }, []);
+  // useEffect(() => {
+  //   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+  //     api_host:
+  //       process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+  //     person_profiles: "identified_only",
+  //     capture_pageview: false,
+  //   });
+  // }, []);
 
   return (
     <PHProvider client={posthog}>
