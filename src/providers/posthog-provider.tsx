d992ea5e44a34ea7c57f5d2425e94dc4dd298b2b
theanthonybrooks <61633975+theanthonybrooks@users.noreply.github.com>
2025-05-18 00:37:08 +0200

    wip: sentry and posthog removal

--- src/providers/posthog-provider.tsx
+++ src/providers/posthog-provider.tsx
@@ -1,54 +1,54 @@
-"use client";
+// "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
-import { usePostHog } from "posthog-js/react";
-import { Suspense, useEffect } from "react";
+// import { usePathname, useSearchParams } from "next/navigation";
+// import { usePostHog } from "posthog-js/react";
+// import { Suspense, useEffect } from "react";
 
-import posthog from "posthog-js";
-import { PostHogProvider as PHProvider } from "posthog-js/react";
+// import posthog from "posthog-js";
+// import { PostHogProvider as PHProvider } from "posthog-js/react";
 
-export function PostHogProvider({ children }: { children: React.ReactNode }) {
-  // useEffect(() => {
-  //   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-  //     api_host:
-  //       process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
-  //     person_profiles: "identified_only",
-  //     capture_pageview: false,
-  //   });
-  // }, []);
+// export function PostHogProvider({ children }: { children: React.ReactNode }) {
+//   // useEffect(() => {
+//   //   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+//   //     api_host:
+//   //       process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+//   //     person_profiles: "identified_only",
+//   //     capture_pageview: false,
+//   //   });
+//   // }, []);
 
-  return (
-    <PHProvider client={posthog}>
-      <SuspendedPostHogPageView />
-      {children}
-    </PHProvider>
-  );
-}
+//   return (
+//     <PHProvider client={posthog}>
+//       <SuspendedPostHogPageView />
+//       {children}
+//     </PHProvider>
+//   );
+// }
 
-function PostHogPageView() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const posthog = usePostHog();
+// function PostHogPageView() {
+//   const pathname = usePathname();
+//   const searchParams = useSearchParams();
+//   const posthog = usePostHog();
 
-  // Track pageviews
-  useEffect(() => {
-    if (pathname && posthog) {
-      let url = window.origin + pathname;
-      if (searchParams.toString()) {
-        url = url + "?" + searchParams.toString();
-      }
+//   // Track pageviews
+//   useEffect(() => {
+//     if (pathname && posthog) {
+//       let url = window.origin + pathname;
+//       if (searchParams.toString()) {
+//         url = url + "?" + searchParams.toString();
+//       }
 
-      posthog.capture("$pageview", { $current_url: url });
-    }
-  }, [pathname, searchParams, posthog]);
+//       posthog.capture("$pageview", { $current_url: url });
+//     }
+//   }, [pathname, searchParams, posthog]);
 
-  return null;
-}
+//   return null;
+// }
 
-function SuspendedPostHogPageView() {
-  return (
-    <Suspense fallback={null}>
-      <PostHogPageView />
-    </Suspense>
-  );
-}
+// function SuspendedPostHogPageView() {
+//   return (
+//     <Suspense fallback={null}>
+//       <PostHogPageView />
+//     </Suspense>
+//   );
+// }
