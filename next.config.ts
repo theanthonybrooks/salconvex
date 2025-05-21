4b64e5b2d9ff5f32fe23efe2d6fa1ba83bafa099
theanthonybrooks <61633975+theanthonybrooks@users.noreply.github.com>
2025-05-21 12:48:59 +0200

    wip: trying with reduced source maps

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
-import {withSentryConfig} from "@sentry/nextjs";
-import type { NextConfig } from "next"
-import path from "path"
+import { withSentryConfig } from "@sentry/nextjs";
+import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   typescript: {
@@ -8,51 +8,50 @@ const nextConfig: NextConfig = {
   },
   webpack: (config) => {
     // Allow module resolution from the root directory
-    config.resolve.modules.push(path.resolve(__dirname))
+    config.resolve.modules.push(path.resolve(__dirname));
 
     // Ensure TypeScript files in convex-helpers are compiled
     config.module.rules.push({
       test: /\.ts$/,
       include: /node_modules\/convex-helpers/,
       use: [{ loader: "ts-loader" }],
-    })
+    });
 
-    return config
+    return config;
   },
   images: {
     unoptimized: true, // 🚀 Completely disables Next.js image optimization
   },
-}
+};
 
 export default withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://www.npmjs.com/package/@sentry/webpack-plugin#options
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-org: "thestreetartlist",
-project: "javascript-nextjs",
+  org: "thestreetartlist",
+  project: "javascript-nextjs",
 
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
 
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: false,
 
-// Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-// tunnelRoute: "/monitoring",
+  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  // tunnelRoute: "/monitoring",
 
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
 
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
 });
-
