1b8c9241a4b401e305ab3c7d7c8c54a71ad10e6e
theanthonybrooks <61633975+theanthonybrooks@users.noreply.github.com>
2025-06-06 03:13:01 -0400

    wip: table filter updates

--- src/components/data-table/data-table-faceted-filter.tsx
+++ src/components/data-table/data-table-faceted-filter.tsx
@@ -1,5 +1,5 @@
 import { Column } from "@tanstack/react-table";
-import { Check, PlusCircle } from "lucide-react";
+import { PlusCircle } from "lucide-react";
 import * as React from "react";
 
 import { Badge } from "@/components/ui/badge";
@@ -20,6 +20,7 @@ import {
 } from "@/components/ui/popover";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
+import { FaCheck } from "react-icons/fa";
 
 interface DataTableFacetedFilterProps<TData, TValue> {
   className?: string;
@@ -64,7 +65,7 @@ export function DataTableFacetedFilter<TData, TValue>({
                 {selectedValues.size}
               </Badge>
               <div className="hidden space-x-1 lg:flex">
-                {selectedValues.size > 2 ? (
+                {selectedValues.size > 1 ? (
                   <Badge
                     variant="basic"
                     className="rounded-sm px-1 font-normal"
@@ -89,10 +90,13 @@ export function DataTableFacetedFilter<TData, TValue>({
           )}
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] border-1.5 p-0" align="start">
+      <PopoverContent
+        className="w-fit min-w-[--radix-popover-trigger-width] border-1.5 p-0"
+        align="start"
+      >
         <Command>
           <CommandInput placeholder={title} />
-          <CommandList>
+          <CommandList className="scrollable mini darkbar justy">
             <CommandEmpty>No results found.</CommandEmpty>
             <CommandGroup>
               {options.map((option) => {
@@ -106,7 +110,7 @@ export function DataTableFacetedFilter<TData, TValue>({
                       } else {
                         selectedValues.add(option.value);
                       }
-                      console.log(selectedValues);
+                      // console.log(selectedValues);
                       const filterValues = Array.from(selectedValues);
                       column?.setFilterValue(
                         filterValues.length ? filterValues : undefined,
@@ -130,13 +134,13 @@ export function DataTableFacetedFilter<TData, TValue>({
                   >
                     <div
                       className={cn(
-                        "mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary",
+                        "mr-2 flex size-4 shrink-0 items-center justify-center rounded-sm border border-primary hover:cursor-pointer",
                         isSelected
                           ? "bg-card text-foreground"
                           : "opacity-50 [&_svg]:invisible",
                       )}
                     >
-                      <Check />
+                      <FaCheck className="size-3 translate-y-[1.1px]" />
                     </div>
                     {option.icon && (
                       <option.icon className="mr-2 h-4 w-4 text-muted-foreground" />
@@ -151,19 +155,48 @@ export function DataTableFacetedFilter<TData, TValue>({
                 );
               })}
             </CommandGroup>
-            {selectedValues.size > 0 && (
-              <>
-                <CommandSeparator />
-                <CommandGroup>
-                  <CommandItem
-                    onSelect={() => column?.setFilterValue(undefined)}
-                    className="justify-center text-center"
-                  >
-                    Clear filters
-                  </CommandItem>
-                </CommandGroup>
-              </>
-            )}
+            <CommandSeparator className="bg-foreground/30" />
+
+            <div className="flex w-full items-center">
+              {selectedValues.size > 0 && (
+                <>
+                  <CommandGroup className="w-full flex-1">
+                    <CommandItem
+                      onSelect={() => column?.setFilterValue(undefined)}
+                      className="justify-center text-center"
+                    >
+                      Clear
+                    </CommandItem>
+                    {/* <Separator
+                      orientation="vertical"
+                      className="mx-1 flex h-full min-h-6"
+                    /> */}
+                  </CommandGroup>
+                </>
+              )}
+
+              {selectedValues.size !== options.length && (
+                <>
+                  {selectedValues.size > 0 && (
+                    <Separator
+                      orientation="vertical"
+                      className="h-full min-h-7 bg-foreground/30"
+                    />
+                  )}
+                  <CommandGroup className="w-full flex-1">
+                    <CommandItem
+                      onSelect={() => {
+                        const allValues = options.map((o) => o.value);
+                        column?.setFilterValue(allValues);
+                      }}
+                      className="justify-center text-center"
+                    >
+                      Select All
+                    </CommandItem>
+                  </CommandGroup>
+                </>
+              )}
+            </div>
           </CommandList>
         </Command>
       </PopoverContent>
