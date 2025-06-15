cdb513a3c32777e38290b7ea0d6015931626a906
theanthonybrooks <61633975+theanthonybrooks@users.noreply.github.com>
2025-06-15 05:33:30 -0400

    fix: removed console log from open call page

--- src/app/(pages)/(artist)/thelist/event/[slug]/[year]/call/page.tsx
+++ src/app/(pages)/(artist)/thelist/event/[slug]/[year]/call/page.tsx
@@ -36,8 +36,6 @@ const OpenCallDetail = () => {
 
   const artistData = useQuery(api.artists.artistActions.getArtistFull);
 
-  console.log(data, isError, error);
-
   useEffect(() => {
     if (isError && !hasRedirected.current) {
       hasRedirected.current = true;
