f4d2408b6bcf262482199595d69d88e137f1e869
theanthonybrooks <61633975+theanthonybrooks@users.noreply.github.com>
2025-06-22 23:55:05 -0400

    wip: corrected page title for 'the list' pages

--- src/app/(pages)/(artist)/thelist/event/[slug]/[year]/call/page.tsx
+++ src/app/(pages)/(artist)/thelist/event/[slug]/[year]/call/page.tsx
@@ -1,5 +1,6 @@
 import OpenCallDetail from "@/app/(pages)/(artist)/thelist/components/open-call-page";
 import { capitalize } from "@/lib/utils";
+import { convexAuthNextjsToken } from "@convex-dev/auth/nextjs/server";
 
 import { fetchQuery } from "convex/nextjs";
 import { api } from "~/convex/_generated/api";
@@ -9,27 +10,30 @@ export async function generateMetadata({
 }: {
   params: Promise<{ slug: string; year: string }>;
 }) {
+  const token = await convexAuthNextjsToken();
   const { slug, year } = await params;
   console.log(slug, year);
 
   try {
-    const event = await fetchQuery(api.events.event.getEventWithOCDetails, {
-      slug,
-      edition: Number(year),
-      source: "ocpage",
-    });
-
-    console.log(event);
+    const event = await fetchQuery(
+      api.events.event.getEventWithOCDetails,
+      {
+        slug,
+        edition: Number(year),
+        source: "ocpage",
+      },
+      { token },
+    );
 
     if (!event?.event?.name) {
-      return { title: "Open Call | Event Not Found" };
+      return { title: "Open Call - Event Not Found" };
     }
 
     return {
-      title: `${capitalize(event.event.name)} (${year}) | Open Call`,
+      title: `${capitalize(event.event.name)} (${year}) - Open Call`,
     };
   } catch {
-    return { title: "Open Call | Error" };
+    return { title: "Open Call - Error" };
   }
 }
 
