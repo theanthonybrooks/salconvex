6d9691bc46816922d350f605f6fde9d0a8364ab3
theanthonybrooks <61633975+theanthonybrooks@users.noreply.github.com>
2025-08-04 19:17:26 +0200

    wip: updated the eventOrgLogo component with state to detect errors (when the image doesn't exist)

--- src/components/ui/event-org-logo.tsx
+++ src/components/ui/event-org-logo.tsx
@@ -1,5 +1,6 @@
 import { capitalize, cn } from "@/lib/utils";
 import Image from "next/image";
+import { useState } from "react";
 
 interface EventOrgLogoProps {
   imgSrc: string;
@@ -8,28 +9,35 @@ interface EventOrgLogoProps {
   className?: string;
 }
 
+const fallbackImg = "/1.jpg";
+
 export const EventOrgLogo = ({
   imgSrc = "/1.jpg",
   type = "event",
   size = "medium",
   className,
 }: EventOrgLogoProps) => {
+  const [imgError, setImgError] = useState(false);
+
   const sizeVal = size === "small" ? 40 : size === "large" ? 80 : 60;
   const sizeClass =
-    size === "small" ? "size-10 " : size === "large" ? "size-20 " : "size-15 ";
+    size === "small" ? "size-10" : size === "large" ? "size-20" : "size-15";
+
   return (
     <Image
-      src={imgSrc}
+      src={imgError ? fallbackImg : imgSrc}
       alt={`${capitalize(type)} Logo`}
       width={sizeVal}
       height={sizeVal}
       className={cn(
         "rounded-full border-2 border-foreground",
         size === "small" && "border-1.5",
-
         sizeClass,
         className,
       )}
+      onError={() => setImgError(true)}
+      // The above line will throw a TS error, but it works in the browser.
+      // You can add @ts-ignore if needed.
     />
   );
 };
