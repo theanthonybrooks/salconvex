6a5392c978c39b8b088301f5063d0b4348bb140d
theanthonybrooks <61633975+theanthonybrooks@users.noreply.github.com>
2025-11-17 10:42:42 +0100

    fix(Zod): Email validation: undo
    Restoring z.email. Tested with the unicode version and while it works to validate, the emails are unable to send via resend.

--- src/constants/zodConsts.ts
+++ src/constants/zodConsts.ts
@@ -13,13 +13,15 @@ export const optionalUrl = z
 
 // - Allows any Unicode letters/numbers plus common email symbols in the local part
 // - Allows Unicode domains and subdomains
-const unicodeEmailRegex =
-  /^[\p{L}\p{N}+._-]+(\.[\p{L}\p{N}+._-]+)*@[\p{L}\p{N}-]+(?:\.[\p{L}\p{N}-]+)*\.[\p{L}]{2,}$/u;
+// const unicodeEmailRegex =
+//   /^[\p{L}\p{N}+._-]+(\.[\p{L}\p{N}+._-]+)*@[\p{L}\p{N}-]+(?:\.[\p{L}\p{N}-]+)*\.[\p{L}]{2,}$/u;
+//note-to-self: Can't use this. I can make it valid for international emails, but then resend doesn't respect them and won't work. For now, just using zod's email validator.
 
-export const unicodeEmail = z
-  .string()
-  .refine((value) => unicodeEmailRegex.test(value), {
-    message: "Must be a valid email address",
-  });
+// export const unicodeEmail = z
+//   .string()
+//   .refine((value) => unicodeEmailRegex.test(value), {
+//     message: "Must be a valid email address",
+//   });
+export const unicodeEmail = z.email("Must be a valid email address");
 
 export const optionalEmail = z.union([z.literal(""), unicodeEmail]).optional();
